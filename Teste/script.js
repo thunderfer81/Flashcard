1b633f0a7ac4ceaa8bb75c1a7f09a2615b67ee93
Fernando Soares <165102007+thunderfer81@users.noreply.github.com>
2024-09-17 14:27:12 -0300

    Add files via upload

---
+++ Teste/script.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Variedades',
+    'Qual o melhor doce do mundo?', 
+    'Gibizão'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França', 
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Física',
+    'Qual a 1ª Lei de Newton', 
+    'Lei da inércia'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
